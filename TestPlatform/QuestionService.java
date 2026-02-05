82377df1d9c6afe1eaad84fe3aded35e3dd43566
Rajath BN <rrajath227@gmail.com>
2026-02-05 21:47:23 +0530

    Core concepts Done

---
+++ TestPlatform/QuestionService.java
@@ -0,0 +1,11 @@
+public class QuestionService {
+    
+ Question[] questions= new Question[5];
+  
+ public void dispalyquestions(){
+    System.out.println(questions[0].getQuestions());
+
+ }
+
+     
+}
